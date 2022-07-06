c0f724b5e9e10dc5f5f028bae5b321d3f3ce8632
Denys Yefimkin <iefimkindenis@gmail.com>
2022-07-06 17:25:13 +0300

    second task

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,18 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const ingredientsListElement = document.querySelector("#ingredients");
+const ingredientsItemElement = ingredients.map(ingredient => {
+  const ingredientItemElement = document.createElement("li");
+  ingredientItemElement.classList.add("item");
+  ingredientItemElement.textContent = ingredient;
+  return ingredientItemElement;
+});
+
+ingredientsListElement.append(...ingredientsItemElement);
