96623ec9663e5d99c296b35b1bb07deff47bd2e3
Denys Yefimkin <iefimkindenis@gmail.com>
2022-07-09 21:11:48 +0300

    Update task-08.js
    Task 8 fixed

--- js/task-08.js
+++ js/task-08.js
@@ -2,8 +2,24 @@ const form = document.querySelector('.login-form')
 
 form.addEventListener('submit', onLoginForm)
 
-
 function onLoginForm(event) {
     event.preventDefault();
+    const formElements = event.currentTarget.elements
+    const email = formElements.email.value;
+    const password = formElements.password.value
+
+    const formData = {
+        email: email,
+        password: password,
+    }
+    console.log(formData)
+    form.reset();
+
+    if (email === '' || password === '') {
+        alert('Пусті поля')
+        form.reset();
+        return;
+    }
 }
+
 
