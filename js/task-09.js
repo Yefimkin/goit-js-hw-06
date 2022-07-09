5285ebffd65e2bb853bdbadf33a5ded66fef944a
Denys Yefimkin <iefimkindenis@gmail.com>
2022-07-09 21:22:59 +0300

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,16 @@
+const spanColor = document.querySelector('.color')
+const btnChangeColor = document.querySelector('.change-color')
+
+btnChangeColor.addEventListener('click', changeColorBtnClick)
+
+
+
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function changeColorBtnClick() {
+  spanColor.textContent = getRandomHexColor()
+  document.body.style.backgroundColor = getRandomHexColor()
 }
